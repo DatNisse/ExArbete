49faa9ffc3606599e32cacfa081574d01049d62a
DatNisse <adde.press@gmail.com>
2021-02-18 13:28:20 +0100

    recreation of server

--- Server/Server/Properties/AssemblyInfo.cs
+++ Server/Server/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyTitle("Server")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyProduct("Server")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0caa886f-e7af-4a68-bda9-b6bd4d1602a2")]
+[assembly: Guid("52d9b066-b0c5-4ee2-ae20-1eacabdb90cc")]
 
 // Version information for an assembly consists of the following four values:
 //
